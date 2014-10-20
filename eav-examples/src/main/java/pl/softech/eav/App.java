9bcde66c56d552b506360c0c2c78e594c930eb85
ssledz <slawomir.sledz@gmail.com>
2014-10-21 00:15:54 +0200

    Added eav-examples module

---
+++ eav-examples/src/main/java/pl/softech/eav/App.java
@@ -0,0 +1,13 @@
+package pl.softech.eav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
